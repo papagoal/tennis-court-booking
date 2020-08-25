7d152b410383742b77231f4336ae5bf26ac7ffcf
papagoal <danhangliu@gmail.com>
2020-08-25 16:04:51 -0500

    Updates

---
+++ precache-manifest.d6122d592353e5ca753ee16fc2f25715.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4230457c5313ca145171c444d645324d",
+    "url": "/tennis-court-booking/index.html"
+  },
+  {
+    "revision": "70deb0108b8b8f77c26d",
+    "url": "/tennis-court-booking/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3a1f3a91854a752f31c9",
+    "url": "/tennis-court-booking/static/css/main.4503f8c6.chunk.css"
+  },
+  {
+    "revision": "70deb0108b8b8f77c26d",
+    "url": "/tennis-court-booking/static/js/2.501558e7.chunk.js"
+  },
+  {
+    "revision": "65d71c29c23ed61c2356dcee6d9321b3",
+    "url": "/tennis-court-booking/static/js/2.501558e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1f3a91854a752f31c9",
+    "url": "/tennis-court-booking/static/js/main.91f17791.chunk.js"
+  },
+  {
+    "revision": "faa517c72977f6e9f84e",
+    "url": "/tennis-court-booking/static/js/runtime-main.1409c0a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tennis-court-booking/static/media/logo.5d5d9eef.svg"
+  }
+]);
